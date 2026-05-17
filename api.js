3b1f8ad9f98888c1729829a5ac926c249a4ff402
SamuelK36br <samuelbernardo091@gmail.com>
2026-05-17 10:54:02 -0300

    api.js

---
+++ api.js
@@ -0,0 +1,13 @@
+export default function api(url) {
+	try {
+		console.time("fetch");
+		const res = await fetch(url);
+		
+		if (!res.ok) throw new Error(`${res.status}: ${res.statusText}`);
+		
+		const data = await res.json();
+		console.timeEnd("fetch");
+		
+		console.log(data);
+	} catch (e) { throw new Error(e) }
+}
